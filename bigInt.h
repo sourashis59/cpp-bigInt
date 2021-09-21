0fff2745fae042245cc1c6793cd5dc9a5eeb639e
Sourashis Mondal <sourashismondal872001@gmail.com>
2021-09-22 01:16:14 +0530

    Deleted test.cpp

--- bigInt.h
+++ bigInt.h
@@ -1,3 +1,30 @@
+/*
+
+*Name : Sourashis Mondal
+*Github link: https://github.com/SourashisHeartHacker/cpp-bigInt
+
+* Details: 
+    This library supports operators and functions for handling big integers. 
+    I have represented big integers using string here.
+
+    This library supports basic arithmetic operators like : +, -, *, /, % , unary - , ++, --
+                                relational operators : <, >, =, >= , <=, == , != 
+                                assignment operator : =, += , -= , *=, /=, %=
+                                member functions : isEven() , isOdd(), pow() 
+                                friend functions: pow(), factorial(), abs() , to_string()
+                                
+                                Supported Constructors:
+                                                        bigInt();
+                                                        bigInt(int n);
+                                                        bigInt(long n);
+                                                        bigInt(long long n);
+                                                        bigInt(const string &s);
+                                                        bigInt(const char *s);
+                                                        bigInt(const bigInt &);                                                    
+
+
+
+*/
 
 #pragma once
 
